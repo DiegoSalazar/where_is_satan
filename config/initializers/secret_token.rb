9c2720a9e04cb1455214935d2dbfb1c383fcff1d
Prelang Builder <builder@prelang.com>
2014-04-23 04:18:19 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WhereIsSatan::Application.config.secret_key_base = '6bd01caf46423d1d98fae706c3add0547995d88cafd7df00e8ca3e2d44de9b031e88ab0357b833aced9b1c134d3cb59fe1c93139095c853194fa57d071246bb4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WhereIsSatan::Application.config.session_store :cookie_store, key: '_where_is_satan_session'
